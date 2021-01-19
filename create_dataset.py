07bd5431d16d39eec82cd20d49e20d9096447367
SIC98 <shininchul98@gmail.com>
2021-01-19 23:42:58 +0900

    Create dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,7 +1,49 @@
 from subprocess import call
+import pickle
+import random
+import math
+import os
 
 with open('top100_repository.txt', 'r') as f:
     lines = f.readlines()
 
 for line in lines:
     call(['git', 'clone', line.strip(), f'resources/{line.strip().split("/")[-1]}'])
+
+total_data = []
+total_files = []
+count = 0
+
+for line in lines:
+    for currentpath, folders, files in os.walk(f'resources/{line.strip().split("/")[-1]}'):
+        for file in files:
+            if file[-3:] == '.py':
+                count += 1
+                total_files.append(os.path.join(currentpath, file))
+
+print('files: ', len(total_files))
+
+for file in total_files:
+    with open(file, "r") as f:
+        try:
+            t = f.readlines()
+        except UnicodeDecodeError:
+            print('DecoderError: ', file)
+        summary = ''.join(t)
+        summary = str(summary).strip()
+        bos_token = '<BOS>'
+        eos_token = '<EOS>'
+        data = bos_token + ' ' + summary + ' ' + eos_token
+        total_data.append(data)
+
+random.shuffle(total_data)
+train_len = math.ceil(len(total_data) * 0.9)
+
+with open("data/data.txt", "wb") as fp:
+    pickle.dump(total_data, fp)
+
+with open("data/train.txt", "wb") as fp:
+    pickle.dump(total_data[:train_len], fp)
+
+with open("data/test.txt", "wb") as fp:
+    pickle.dump(total_data[train_len:], fp)
