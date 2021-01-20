c9314f762d52e7f52fb0133c86e57fa9cf4ccb1c
SIC98 <shininchul98@gmail.com>
2021-01-20 23:51:17 +0900

    Data type: txt to json

--- create_dataset.py
+++ create_dataset.py
@@ -2,15 +2,16 @@ from subprocess import call
 import pickle
 import random
 import math
+import json
 import os
 
 with open('top100_repository.txt', 'r') as f:
     lines = f.readlines()
 
-for line in lines:
-    call(['git', 'clone', line.strip(), f'resources/{line.strip().split("/")[-1]}'])
+# for line in lines:
+#     call(['git', 'clone', line.strip(), f'resources/{line.strip().split("/")[-1]}'])
 
-total_data = []
+json_data = {}
 total_files = []
 count = 0
 
@@ -23,6 +24,7 @@ for line in lines:
 
 print('files: ', len(total_files))
 
+idx = 0
 for file in total_files:
     with open(file, "r") as f:
         try:
@@ -33,17 +35,23 @@ for file in total_files:
         summary = str(summary).strip()
         bos_token = '<BOS>'
         eos_token = '<EOS>'
-        data = bos_token + ' ' + summary + ' ' + eos_token
-        total_data.append(data)
+        data = bos_token + summary + eos_token
+        json_data[idx] = data
+        idx += 1
 
-random.shuffle(total_data)
-train_len = math.ceil(len(total_data) * 0.9)
+keys = list(json_data.keys())
+random.shuffle(keys)
 
-with open("data/data.txt", "w") as fp:
-    fp.write('\n'.join(total_data))
+train_len = math.ceil(len(keys) * 0.9)
 
-with open("data/train.txt", "w") as fp:
-    fp.write('\n'.join(total_data[:train_len]))
+train_dict = {k: json_data[k] for k in keys[:train_len]}
+test_dict = {k: json_data[k] for k in keys[train_len:]}
 
-with open("data/test.txt", "w") as fp:
-    fp.write('\n'.join(total_data[train_len:]))
+with open("data/data.json", "w") as fp:
+    json.dump(json_data, fp, indent=4)
+
+with open("data/train.json", "w") as fp:
+    json.dump(train_dict, fp, indent=4)
+
+with open("data/test.json", "w") as fp:
+    json.dump(test_dict, fp, indent=4)
